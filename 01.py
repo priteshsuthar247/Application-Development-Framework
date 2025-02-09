a00d5edec6ede27bf000e3164876d094c5ea36d9
pritesh-suthar <pritesh.suthar247@gmail.com>
2025-02-10 01:03:12 +0530

    corrected grammer

--- 01.py
+++ 01.py
@@ -5,6 +5,6 @@ import random
 import math
 
 radius = random.randrange(0,99,5)
-print(f"Radius of th circle is: {radius}")
+print(f"Radius of the circle is: {radius}")
 area = math.pi * pow(radius,2)
 print(f"Area of the circle is: {area}")
