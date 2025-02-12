1ae13dbacc36bf67c4a2f7ebd22e1c0f26d3bc1b
pritesh-suthar <pritesh.suthar247@gmail.com>
2025-02-12 12:46:20 +0530

    added

--- 05.py
+++ 05.py
@@ -1,2 +1,19 @@
 # Aim
 # Write a Python program that retrieves data from a SQL database and saves it as a CSV file.
+
+import mysql.connector
+from mysql.connector import Error
+
+connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            passwd='root',
+            database='testdb'
+        )
+cursor = connection.cursor()
+query = "SELECT * FROM STORE"
+cursor.execute(query)
+results = cursor.fetchall()
+for row in results:
+    print(row)
+cursor.close()
