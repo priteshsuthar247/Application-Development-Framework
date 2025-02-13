45519c08f702c10c6c66a8d1d65adf4e4a779315
pritesh-suthar <pritesh.suthar247@gmail.com>
2025-02-13 10:50:11 +0530

    removed empty lines

--- 05/05.py
+++ 05/05.py
@@ -1,8 +1,7 @@
 # Aim
 # Write a Python program that retrieves data from a SQL database and saves it as a CSV file.
-
+import csv
 import mysql.connector
-from mysql.connector import Error
 
 connection = mysql.connector.connect(
             host='localhost',
@@ -15,9 +14,6 @@ query1 = "SELECT * FROM STORE;"
 cursor.execute(query1)
 results1 = cursor.fetchall()
 
-
-   
-
 query2 = "desc STORE;"
 cursor.execute(query2)
 results2 = cursor.fetchall()
@@ -28,9 +24,6 @@ for row in results2:
 print(header)
 file = open("05//dataset.csv",'w+',newline='')
 
-import csv
-
 csv_writer = csv.writer(file)
-
 csv_writer.writerow(header)
 csv_writer.writerows(results1)
