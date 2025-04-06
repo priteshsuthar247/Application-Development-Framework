1e4075c6527a37399ab48db5fa9f895d7940bf10
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2025-04-06 14:29:13 +0000

    updated

--- 13/firebase_control/firebase_config.py
+++ 13/firebase_control/firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Initialize Firebase
-cred = credentials.Certificate("13/firebase_control/firebase_key.json")
+cred = credentials.Certificate("firebase_key.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://led-controller-697a9-default-rtdb.firebaseio.com/'
 })
